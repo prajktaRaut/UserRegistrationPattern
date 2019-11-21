530a76360663b62dca78db173f4653341cc37c93
prajktaRaut <prajktasraut16@gmail.com>
2019-11-21 11:23:03 +0530

    Function for valid email_id

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -35,5 +35,24 @@ function checkLastName()
 
 }
 
+function checkEmail()
+{
+
+	echo "Enter email tocheck valid or not "
+
+	read email
+
+	pattern="^([a-zA-Z0-9]+)([+_-.][a-zA-Z0-9]+)?[@][a-zA-Z]+([.][a-z]{2,3})([.][a-z]{2})?$"
+
+	if [[ $email =~ $pattern ]];
+	then
+        	echo "Valid"
+	else
+        	echo "Invalid"
+	fi
+
+}
+
 checkFirstName
 checkLastName
+checkEmail
