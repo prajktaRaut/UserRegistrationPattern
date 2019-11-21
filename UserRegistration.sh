eaa4fb2083a64c55563a95a98d8a5dfa6c14efad
prajktaRaut <prajktasraut16@gmail.com>
2019-11-21 10:53:36 +0530

    To check firstname is valid or not

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 
 echo "Welcome to user registration system"
+
+echo "Enter your first name"
+
+read firstName
+
+pattern="^[A-Z][a-z]{2}"
+
+if [[ $firstName =~ $pattern ]];
+then
+        echo "Valid"
+else
+        echo "Invalid"
+fi
+
+
