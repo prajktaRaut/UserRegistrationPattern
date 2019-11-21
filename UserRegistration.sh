8442c1d02a685fe0b46c4d9e039f4f032cabdd18
prajktaRaut <prajktasraut16@gmail.com>
2019-11-21 11:10:32 +0530

    Function to check valid lastname

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,17 +2,38 @@
 
 echo "Welcome to user registration system"
 
-echo "Enter your first name"
+function checkFirstName()
+{
+	echo "Enter your first name"
 
-read firstName
+	read firstName
 
-pattern="^[A-Z][a-z]{2}"
+	pattern="^[A-Z][a-z]{2}"
 
-if [[ $firstName =~ $pattern ]];
-then
-        echo "Valid"
-else
-        echo "Invalid"
-fi
+	if [[ $firstName =~ $pattern ]];
+	then
+        	echo "Valid"
+	else
+        	echo "Invalid"
+	fi
+}
 
+function checkLastName()
+{
+	echo "Enter your last name"
 
+	read lastName
+
+	pattern="^[A-Z][a-z]{2}"
+
+	if [[ $lastName =~ $pattern ]];
+	then
+        	echo "Valid"
+	else
+        	echo "Invalid"
+	fi
+
+}
+
+checkFirstName
+checkLastName
